0bb4871addc66797c4561114db1aee5098c035c8
janisollo <sollo.jani@gmail.com>
2022-03-14 15:35:16 +0200

    eka päivä pulkassa

---
+++ t1.js
@@ -0,0 +1,9 @@
+let fname = "Jani";
+
+console.log("Terve " + fname);
+
+let age = 15;
+const lenght = 182;
+
+console.log(fname + " on " + age + " vuotias");
+console.log(fname + " on " + age + " vuotias ja hänellä on pituutta " + lenght);
